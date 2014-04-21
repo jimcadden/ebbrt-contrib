fd77fb7b2ac3f5faf0b8449f92aed78e9532153b
James Cadden <jmcadden@bu.edu>
2014-04-21 01:24:07 -0400

    payload size added to memaslap batch script

--- scripts/memcached-benchmarks/memaslap.py
+++ scripts/memcached-benchmarks/memaslap.py
@@ -15,15 +15,16 @@ if "check_output" not in dir( subprocess ): # duck punch it in!
             cmd = kwargs.get("args")
             if cmd is None:
                 cmd = popenargs[0]
-            raise subprocess.CalledProcessError()
+            print cmd
+            raise subprocess.CalledProcessError(retcode,cmd)
         return output
     subprocess.check_output = f
 #####
 ''' 
     EBBRT_MEMASLAP_CMD  - base command that will be augmented with each trial.
                           Use the placeholder "{IP}" 
-    EBBRT_VM_BOOT - script to bring up vm, return IP
-    EBBRT_VM_CLEAN - TODO...
+    EBBRT_VMBOOT_CMD - script to bring up vm, return IP
+    EBBRT_VMCLEAN_CMD - TODO...
 '''
 
 if len(sys.argv) != 4:
@@ -36,15 +37,14 @@ trial_count = sys.argv[3]
 exp_logs = os.path.join(os.getcwd(),exp_name+'.log')
 exp_cols = os.path.join(os.getcwd(),exp_name+'.data')
 
-base_cmd = os.environ['EBBRT_MEMASLAP_CMD'] 
-vm_boot_cmd = os.envoirn['EBBRT_VM_BOOT']
-
 end_exp = False
 i = 0
 
 def cmd_flag(test, count):
     if test == "concurrency":
       return "-c"
+    elif test == "payload":
+      return "-X"
     elif test == "singleton":
       return ""
     else:
@@ -54,39 +54,60 @@ def cmd_flag(test, count):
 def cmd_val(test, count):
     if test == "concurrency":
       return str((4 * (count+1) * (count+1)))
+    elif test == "payload":
+      return str(64 * (count+1))
     elif test == "singleton":
       return ""
     else:
       print "error: unknown experiment type", test
       exit(0)
 
-def build_cmd(test, count):
-    retstr = " "+str(cmd_flag(test, count))+" "+str(cmd_val(test,count))
-    ip = subprocess.check_output(vm_boo_cmd, shell=True)
-    retstr.replace('{IP}', ip) 
-    print retstr
-    exit(0)
-    return retstr
-
 def continue_exp(test, count):
     if test == "concurrency":
       return (count < 13) # 4 *(13^2) = 676
+    elif test == "payload":
+      return (count < 32) # 64 * 32 = 2048 
     elif test == "singleton":
       return (count < 1)
     else:
       print "Error: unknown experiment type", test
       exit(0)
 
+def build_cmd(test, count):
+    ip = subprocess.check_output(vm_boot_cmd, shell=True)
+    ip_clean = ip.rstrip('\n')
+    if not ip_clean:
+      print "Error, we were unable to get an IP after boot."
+      exit(0)
+    retstr = str(base_cmd+" "+str(cmd_flag(test, count))+" "+str(cmd_val(test,count)))
+    return retstr.replace("{IP}", ip_clean) 
+
+
 if os.path.isfile(exp_logs) or os.path.isfile(exp_cols):
     print "An experiment in this directory with this name already exists."
     print "Aborting..."
     exit(0)
+
+################################################################################
+
 try: 
-    trial_cmd = os.environ['EBBRT_MEMASLAP_CMD']
+    base_cmd = os.environ['EBBRT_MEMASLAP_CMD'] 
+    if not base_cmd:
+      raise KeyError()
 except KeyError:
     print "Missing envoirnment variable: EBBRT_MEMASLAP_CMD"
     print "Aborting..."
     exit(0)
+try: 
+    vm_boot_cmd = os.environ['EBBRT_VMBOOT_CMD']
+    if not vm_boot_cmd:
+      raise KeyError()
+except KeyError:
+    print "Missing envoirnment variable: EBBRT_VMBOOT_CMD"
+    print "Aborting..."
+    exit(0)
+
+
 
 with open(exp_logs, 'w') as log:
   with open(exp_cols, 'w') as data:
