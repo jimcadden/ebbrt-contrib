c8a82b84783d3218a2d0048195daeaa0ea2791b8
Jim Cadden <jmcadden@bu.edu>
2016-04-08 19:13:23 -0400

    Create general purpose counters

--- apps/sanity/baremetal/src/Perf.cc
+++ apps/sanity/baremetal/src/Perf.cc
@@ -10,51 +10,102 @@
 #include "Perf.h"
 
 namespace {
-thread_local uint64_t  perf_global_ctrl{0};
+thread_local uint64_t perf_global_ctrl{0};
+thread_local uint8_t  next_pmc{0};
 };
 
 ebbrt::perf::PerfCounter::PerfCounter(ebbrt::perf::PerfEvent evt) : evt_{evt}, is_running_{false} {
 
-  uint32_t a,b;
-  ebbrt::cpuid(PERF_CPUID_LEAF,0,&a, &b, &b, &b);
+  uint32_t a,b,c,d;
+  ebbrt::cpuid(PERF_CPUID_LEAF,0,&a, &b, &c, &d);
   pmc_version_ = a & 0xFF;
-  pmc_gp_count_ = (a>>8) & 0xFF; 
-  pmc_events_ = (a>>16) & 0xFF; 
-  kprintf("PMC: v%d #%d %x\n", pmc_version_, pmc_gp_count_, pmc_events_);
-  kassert( pmc_gp_count_ > 0 );
+  pmc_count_ = (a>>8) & 0xFF; 
+  pmc_width_ = (a>>16) & 0xFF; 
+  pmc_events_ = (b) & 0xFF; 
+  pmc_fixed_count_ = (d) & 0xF; 
+  pmc_fixed_width_ = (d>>4) & 0xFF; 
 
-  kprintf("EVT: %x\n", evt_);
+  kassert( pmc_version_  >= 1 );
+  kassert( pmc_count_ > 0 );
+  if( (uint8_t)evt_ >= FIXED_EVT_OFFSET )
+    kassert( pmc_version_  >= 2 );
+  else{
+    auto not_valid = (pmc_events_ >> (uint8_t)evt_);
+    kassert( not_valid == 0);
+  }
+
   ia32_fixed_ctr_ctrl_t fixed_ctrl;
-  fixed_ctrl.val = 0;
-  //fixed_ctrl.val = ebbrt::rdmsr(IA32_FIXED_CTR_CTRL);
+  ia32_perfevtsel_t perfevtsel;
+  fixed_ctrl.val = ebbrt::rdmsr(IA32_FIXED_CTR_CTRL_MSR); 
+  perfevtsel.val = 0;
+  perfevtsel.usermode = 1;
+  perfevtsel.osmode = 1;
+  //perfevtsel.en = 1;
   switch(evt_){
+    case PerfEvent::cycles:
+      perfevtsel.event = PERFEVTSEL_EVT_CYCLES;
+      perfevtsel.umask = PERFEVTSEL_UMASK_CYCLES;
+      ebbrt::wrmsr(perfevtsel.val, IA32_PERFEVTSEL_MSR(next_pmc));
+      next_pmc++;
+      break;
+    case PerfEvent::instructions:
+      perfevtsel.event = PERFEVTSEL_EVT_INSTRUCTIONS;
+      perfevtsel.umask = PERFEVTSEL_UMASK_INSTRUCTIONS;
+      ebbrt::wrmsr(perfevtsel.val, IA32_PERFEVTSEL_MSR(next_pmc));
+      next_pmc++;
+      break;
+    case PerfEvent::reference_cycles:
+      perfevtsel.event = PERFEVTSEL_EVT_CYCLES_REF;
+      perfevtsel.umask = PERFEVTSEL_UMASK_CYCLES_REF;
+      ebbrt::wrmsr(perfevtsel.val, IA32_PERFEVTSEL_MSR(next_pmc));
+      next_pmc++;
+      break;
+    case PerfEvent::llc_references:
+      perfevtsel.event = PERFEVTSEL_EVT_LLC_REF;
+      perfevtsel.umask = PERFEVTSEL_UMASK_LLC_REF;
+      ebbrt::wrmsr(perfevtsel.val, IA32_PERFEVTSEL_MSR(next_pmc));
+      next_pmc++;
+      break;
+    case PerfEvent::llc_misses:
+      perfevtsel.event = PERFEVTSEL_EVT_LLC_MISSES;
+      perfevtsel.umask = PERFEVTSEL_UMASK_LLC_MISSES;
+      ebbrt::wrmsr(perfevtsel.val, IA32_PERFEVTSEL_MSR(next_pmc));
+      next_pmc++;
+      break;
+    case PerfEvent::branch_instructions:
+      perfevtsel.event = PERFEVTSEL_EVT_BRANCH_INSTRUCTIONS;
+      perfevtsel.umask = PERFEVTSEL_UMASK_BRANCH_INSTRUCTIONS;
+      ebbrt::wrmsr(perfevtsel.val, IA32_PERFEVTSEL_MSR(next_pmc));
+      next_pmc++;
+      break;
+    case PerfEvent::branch_misses:
+      perfevtsel.event = PERFEVTSEL_EVT_BRANCH_MISSES;
+      perfevtsel.umask = PERFEVTSEL_UMASK_BRANCH_MISSES;
+      ebbrt::wrmsr(perfevtsel.val, IA32_PERFEVTSEL_MSR(next_pmc));
+      next_pmc++;
+      break;
     case PerfEvent::fixed_instructions: 
-      kassert( pmc_version_  >= 2 );
-      fixed_ctrl.ctr0_enable = 3;
+      fixed_ctrl.ctr0_enable = FIXED_CTL_RING_LVL_ALL;
       offset_ = ebbrt::rdpmc((FAST_READ(0)));
+      ebbrt::wrmsr(fixed_ctrl.val, IA32_FIXED_CTR_CTRL_MSR);
       break;
     case PerfEvent::fixed_cycles:
-      kassert( pmc_version_  >= 2 );
-      fixed_ctrl.ctr1_enable = 3;
+      fixed_ctrl.ctr1_enable = FIXED_CTL_RING_LVL_ALL;
       offset_ = ebbrt::rdpmc((FAST_READ(1)));
+      fixed_ctrl.val |= ebbrt::rdmsr(IA32_FIXED_CTR_CTRL_MSR); 
+      ebbrt::wrmsr(fixed_ctrl.val, IA32_FIXED_CTR_CTRL_MSR);
       break;
     case PerfEvent::fixed_reference_cycles: 
-      kassert( pmc_version_  >= 2 );
-      fixed_ctrl.ctr2_enable = 3;
+      fixed_ctrl.ctr2_enable = FIXED_CTL_RING_LVL_ALL;
       offset_ = ebbrt::rdpmc((FAST_READ(2)));
+      fixed_ctrl.val |= ebbrt::rdmsr(IA32_FIXED_CTR_CTRL_MSR); 
+      ebbrt::wrmsr(fixed_ctrl.val, IA32_FIXED_CTR_CTRL_MSR);
       break;
     default:
-      ebbrt::kbugon("PMCv.1 not yet supported");
+       kbugon("Error: unknown event type");
       break;
-      //ebbrt::perf::ia32_perf_global_t ctrl;
-      // ebbrt::wrmsr(ctrl.val, IA32_PERF_GLOBAL_CTRL);
   };
-
-  if( fixed_ctrl.val != 0 ){
-    auto tmp = ebbrt::rdmsr(IA32_FIXED_CTR_CTRL);
-    fixed_ctrl.val |= tmp; 
-    ebbrt::wrmsr(fixed_ctrl.val, IA32_FIXED_CTR_CTRL);
-  }
+  return;
 }
 
 void ebbrt::perf::PerfCounter::Clear(){
@@ -73,16 +124,20 @@ void ebbrt::perf::PerfCounter::Clear(){
   };
 }
 
+int8_t ebbrt::perf::PerfCounter::AvailablePMCs(){
+  return (( (int8_t)pmc_count_ - (int8_t)next_pmc ));
+}
+
 void ebbrt::perf::PerfCounter::Start(){
   auto shl = static_cast<uint8_t>(evt_);
   perf_global_ctrl |= (1ull << shl); 
-  ebbrt::wrmsr(perf_global_ctrl, IA32_PERF_GLOBAL_CTRL);
+  ebbrt::wrmsr(perf_global_ctrl, IA32_PERF_GLOBAL_CTRL_MSR);
 }
 
 void ebbrt::perf::PerfCounter::Stop(){
   auto shl = static_cast<uint8_t>(evt_);
   perf_global_ctrl &= ~(1ull << shl); 
-  ebbrt::wrmsr(perf_global_ctrl, IA32_PERF_GLOBAL_CTRL);
+  ebbrt::wrmsr(perf_global_ctrl, IA32_PERF_GLOBAL_CTRL_MSR);
 }
 uint64_t ebbrt::perf::PerfCounter::Read(){
   switch(evt_){
--- apps/sanity/baremetal/src/Perf.h
+++ apps/sanity/baremetal/src/Perf.h
@@ -9,33 +9,37 @@
 namespace ebbrt {
 namespace perf {
 #define PERF_CPUID_LEAF                       0x0A
-#define FAST_READ(X)                             ((1 << 30)+X)
+#define FIXED_CTL_RING_LVL_OS                 0x1
+#define FIXED_CTL_RING_LVL_USR                0x2
+#define FIXED_CTL_RING_LVL_ALL                0x3
+#define FAST_READ(X)                          ((1 << 30)+X)
+#define FIXED_EVT_OFFSET                      0x20
 
-#define IA32_FIXED_CTR_CTRL                   0x38D
-#define IA32_PERF_GLOBAL_CTRL                 0x38F
-#define IA32_PERF_GLOBAL_STATUS               0x38E
-#define IA32_PERF_GLOBAL_OVF_CTRL             0x390
-#define IA32_PERFEVTSEL(X)                    (0x186+(X))
+#define IA32_FIXED_CTR_CTRL_MSR               0x38D
+#define IA32_PERF_GLOBAL_CTRL_MSR             0x38F
+#define IA32_PERF_GLOBAL_STATUS_MSR           0x38E
+#define IA32_PERF_GLOBAL_OVF_CTRL_MSR         0x390
 #define IA32_FXD_CTR(X)                       (0x309+(X))
-#define IA32_GP_CTR(X)                        (0x4C1+(X))
 #define PERF_EVENT_BITMASK(X)                 (1 << X)
 
-#define PERFEVT_UMASK_CYCLES                  0x00
-#define PERFEVT_UMASK_CYCLES_REF              0x00
-#define PERFEVT_UMASK_INSTRUCTIONS            0x01
-#define PERFEVT_UMASK_LLC_REF                 0x4f
-#define PERFEVT_UMASK_LLC_MISSES              0x41
-#define PERFEVT_UMASK_BRANCH_INSTRUCTIONS     0x00
-#define PERFEVT_UMASK_BRANCH_MISSES           0x00
+#define IA32_PERFEVTSEL_MSR(X)                (0x186+(X))
+#define IA32_PERFEVTCNT_MSR                   0x4C1
+#define IA32_GP_CTR(X)                        (0x4C1+(X))
 
-#define PERFEVT_SEL_CYCLES                    0x3C
-#define PERFEVT_SEL_CYCLES_REF                0xC0
-#define PERFEVT_SEL_INSTRUCTIONS              0x3C
-#define PERFEVT_SEL_LLC_REF                   0x2E
-#define PERFEVT_SEL_LLC_MISSES                0x2E
-#define PERFEVT_SEL_BRANCH_INSTRUCTIONS       0xC4
-#define PERFEVT_SEL_BRANCH_MISSES             0xC5
-#define FIXED_CTR_OFFSET                      0x20
+#define PERFEVTSEL_EVT_CYCLES                 0x3C
+#define PERFEVTSEL_EVT_INSTRUCTIONS           0xC0
+#define PERFEVTSEL_EVT_CYCLES_REF             0x3C
+#define PERFEVTSEL_EVT_LLC_REF                0x2E
+#define PERFEVTSEL_EVT_LLC_MISSES             0x2E
+#define PERFEVTSEL_EVT_BRANCH_INSTRUCTIONS    0xC4
+#define PERFEVTSEL_EVT_BRANCH_MISSES          0xC5
+#define PERFEVTSEL_UMASK_CYCLES               0x00
+#define PERFEVTSEL_UMASK_INSTRUCTIONS         0x00
+#define PERFEVTSEL_UMASK_CYCLES_REF           0x01
+#define PERFEVTSEL_UMASK_LLC_REF              0x4f
+#define PERFEVTSEL_UMASK_LLC_MISSES           0x41
+#define PERFEVTSEL_UMASK_BRANCH_INSTRUCTIONS  0x00
+#define PERFEVTSEL_UMASK_BRANCH_MISSES        0x00
 
 enum class PerfEvent : uint8_t {
     cycles = 0x0,
@@ -45,7 +49,7 @@ enum class PerfEvent : uint8_t {
     llc_misses,
     branch_instructions,
     branch_misses,
-    fixed_instructions = FIXED_CTR_OFFSET,
+    fixed_instructions = FIXED_EVT_OFFSET,
     fixed_cycles,
     fixed_reference_cycles
 };
@@ -54,8 +58,8 @@ typedef struct {
   union {
     uint64_t val;
     struct {
-      uint64_t eventselect_7_0 : 8;
-      uint64_t unitmask : 8;
+      uint64_t event : 8;
+      uint64_t umask : 8;
       uint64_t usermode : 1;
       uint64_t osmode : 1;
       uint64_t edge : 1;
@@ -121,13 +125,18 @@ public:
   uint64_t Read();
   void Start();
   void Stop();
+  int8_t AvailablePMCs();
 private:
   PerfEvent evt_;
   bool is_running_;
   uint64_t offset_;
-  uint8_t pmc_gp_count_;
   uint8_t pmc_version_;
+  uint8_t pmc_count_;
+  uint8_t pmc_width_;
   uint8_t pmc_events_;
+  uint8_t pmc_fixed_count_;
+  uint8_t pmc_fixed_width_;
+  uint8_t overflow_cnt;
 };
 
 }
