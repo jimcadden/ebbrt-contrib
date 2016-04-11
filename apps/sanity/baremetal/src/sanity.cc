437f881b51048572ff4274f3ba632eccce143a91
Jim Cadden <jmcadden@bu.edu>
2016-04-11 16:54:06 -0400

    Sanity counters in unordered map

--- apps/sanity/baremetal/src/sanity.cc
+++ apps/sanity/baremetal/src/sanity.cc
@@ -6,91 +6,31 @@
 #define PRINT_ITERATIONS 100
 #define NIL_ITERATIONS 10000
 
+#include <unordered_map>
 #include <ebbrt/Acpi.h>
 #include <ebbrt/EventManager.h>
 #include <ebbrt/Perf.h>
-//#include "Perf.h"
 #include "Printer.h"
 
 void AppMain() { 
+
   printer->Print("SANITY BACKEND UP.\n"); 
+  std::unordered_map<std::string, ebbrt::perf::PerfCounter> counter;
 
-  ebbrt::perf::PerfCounter c{ebbrt::perf::PerfEvent::llc_misses};
-  ebbrt::perf::PerfCounter i{ebbrt::perf::PerfEvent::branch_misses};
-  ebbrt::perf::PerfCounter m{ebbrt::perf::PerfEvent::reference_cycles};
-  // llc_references
-  // reference_cycles
 
-  auto count = c.Read();
-  auto count2 = i.Read();
-  auto count3 = m.Read();
-  ebbrt::kprintf("New: %llu %llu %llu \n", count, count2, count3);
+  counter.emplace(std::make_pair(std::string("cycles"), 
+        std::move(ebbrt::perf::PerfCounter{ebbrt::perf::PerfEvent::cycles})));
+  counter.emplace(std::make_pair(std::string("instructions"), 
+        std::move(ebbrt::perf::PerfCounter{ebbrt::perf::PerfEvent::instructions})));
 
-  c.Start();
-  i.Start();
-  m.Start();
+  //counter.emplace(std::make_pair(std::string("llc_misses"), 
+  //      std::move(ebbrt::perf::PerfCounter{ebbrt::perf::PerfEvent::llc_misses})));
+  //counter.emplace(std::make_pair(std::string("branch_misses"), 
+  //      std::move(ebbrt::perf::PerfCounter{ebbrt::perf::PerfEvent::branch_misses})));
+
   for( int i = 1; i<NIL_ITERATIONS; i++){
     asm volatile(""); 
   }
-  c.Stop();
-  i.Stop();
-  m.Stop();
-
-  count = c.Read();
-  count2 = i.Read();
-  count3 = m.Read();
-  ebbrt::kprintf("Nil: %llu %llu %llu \n", count, count2, count3);
-
-  /// this should not be counted
-  for( int i = 1; i<NIL_ITERATIONS*100; i++){
-    asm volatile(""); 
-  }
-  //// 
-  count = c.Read();
-  count2 = i.Read();
-  count3 = m.Read();
-  ebbrt::kprintf("Stopped: %llu %llu %llu \n", count, count2, count3);
-  
-  c.Start();
-  i.Start();
-  m.Start();
-  for( int i = 1; i<NIL_ITERATIONS; i++){
-    asm volatile(""); 
-  }
-  c.Stop();
-  i.Stop();
-  m.Stop();
-
-  count = c.Read();
-  count2 = i.Read();
-  count3 = m.Read();
-  ebbrt::kprintf("Nil 2: %llu %llu %llu \n", count, count2, count3);
-
-  c.Clear();
-  i.Clear();
-  m.Clear();
-  count = c.Read();
-  count2 = i.Read();
-  count3 = m.Read();
-  ebbrt::kprintf("Clear: %llu %llu %llu \n", count, count2, count3);
-
-
-  c.Start();
-  i.Start();
-  m.Start();
-  for( int i = 1; i<PRINT_ITERATIONS; i++){
-    ebbrt::kprintf(".");
-  }
-  c.Stop();
-  i.Stop();
-  m.Stop();
-
-  count = c.Read();
-  count2 = i.Read();
-  count3 = m.Read();
-  ebbrt::kprintf("\nPrint: %llu %llu %llu \n", count, count2, count3);
-
-
 
   printer->Print("SANITY BACKEND FINISHED.\n"); 
   ebbrt::event_manager->SpawnLocal([=]() { ebbrt::kprintf("Shuting Down...\n"); });
