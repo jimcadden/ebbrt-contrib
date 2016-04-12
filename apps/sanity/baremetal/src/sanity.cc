66516474a04e81857609bad28cff41cd01ec2040
Jim Cadden <jmcadden@bu.edu>
2016-04-11 22:02:39 -0400

    Adds sanity tests

--- apps/sanity/baremetal/src/sanity.cc
+++ apps/sanity/baremetal/src/sanity.cc
@@ -3,8 +3,8 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#define PRINT_ITERATIONS 100
-#define NIL_ITERATIONS 10000
+#define PAGES               10
+#define NIL_ITERATIONS 1000000
 
 #include <unordered_map>
 #include <ebbrt/Acpi.h>
@@ -14,25 +14,53 @@
 
 void AppMain() { 
 
-  printer->Print("SANITY BACKEND UP.\n"); 
-  std::unordered_map<std::string, ebbrt::perf::PerfCounter> counter;
+  printer->Print("BACKEND UP.\n"); 
 
+  uint64_t data[64 * PAGES];
+  for( auto i=0ull; i<(64*PAGES); i++){
+    data[i] = i;
+  }
+  ebbrt::kprintf("array allocated: %d - %d\n", data[0], data[64*PAGES-1]);
 
-  counter.emplace(std::make_pair(std::string("cycles"), 
-        std::move(ebbrt::perf::PerfCounter{ebbrt::perf::PerfEvent::cycles})));
-  counter.emplace(std::make_pair(std::string("instructions"), 
+  std::unordered_map<std::string, ebbrt::perf::PerfCounter> counters;
+  counters.emplace(std::make_pair(std::string("ref_cycles"), 
+        ebbrt::perf::PerfCounter{ebbrt::perf::PerfEvent::reference_cycles}));
+  counters.emplace(std::make_pair(std::string("instructions"), 
         std::move(ebbrt::perf::PerfCounter{ebbrt::perf::PerfEvent::instructions})));
+  counters.emplace(std::make_pair(std::string("llc_misses"), 
+        std::move(ebbrt::perf::PerfCounter{ebbrt::perf::PerfEvent::llc_misses})));
+  counters.emplace(std::make_pair(std::string("branch_misses"), 
+        std::move(ebbrt::perf::PerfCounter{ebbrt::perf::PerfEvent::branch_misses})));
 
-  //counter.emplace(std::make_pair(std::string("llc_misses"), 
-  //      std::move(ebbrt::perf::PerfCounter{ebbrt::perf::PerfEvent::llc_misses})));
-  //counter.emplace(std::make_pair(std::string("branch_misses"), 
-  //      std::move(ebbrt::perf::PerfCounter{ebbrt::perf::PerfEvent::branch_misses})));
-
-  for( int i = 1; i<NIL_ITERATIONS; i++){
-    asm volatile(""); 
+  ebbrt::kprintf("\nInital counter values:\n");
+  for( auto& i : counters ){
+    ebbrt::kprintf("%s\t\t%llu\n", i.first.c_str(), i.second.Read());
   }
 
-  printer->Print("SANITY BACKEND FINISHED.\n"); 
+  ebbrt::kprintf("\n Nil counter:\n");
+  for( auto& i : counters ){
+    i.second.Start();
+    for( int i = 1; i<NIL_ITERATIONS; i++){
+      asm volatile(""); 
+    }
+    i.second.Stop();
+    ebbrt::kprintf("%s\t\t%llu\n", i.first.c_str(), i.second.Read());
+    i.second.Clear();
+  }
+
+  ebbrt::kprintf("\n Sum matrix counter:\n");
+  for( auto& i : counters ){
+    i.second.Start();
+    auto sum = 0ull;
+    for( auto j=0ull; j<(64*PAGES); j++){
+      sum += data[j];
+    }
+    i.second.Stop();
+    ebbrt::kprintf("%s\t\t%llu\t\t%llu\n", i.first.c_str(), i.second.Read(), sum);
+    i.second.Clear();
+  }
+
+  printer->Print("BACKEND FINISHED.\n"); 
   ebbrt::event_manager->SpawnLocal([=]() { ebbrt::kprintf("Shuting Down...\n"); });
   ebbrt::event_manager->SpawnLocal([=]() { ebbrt::acpi::PowerOff(); });
  }
