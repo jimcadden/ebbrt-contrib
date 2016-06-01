b7536a0dc47083e7f962e6bb938d17275cc2106c
Jim Cadden <jmcadden@bu.edu>
2016-06-01 16:21:01 -0400

    app(Socket): remove build flags

--- apps/socket/baremetal/build/build.mk
+++ apps/socket/baremetal/build/build.mk
@@ -20,7 +20,7 @@ lwip_objects := $(patsubst $(MYDIR)../ext/lwip/api/%.c, %.o, $(lwip_api_sources)
 EBBRT_APP_OBJECTS := socket.o Printer.o SocketManager.o Vfs.o lwip_socket_api.o ZooKeeper.o  $(zk_objects) $(lwip_objects)
 EBBRT_APP_VPATH := $(abspath $(MYDIR)../src) $(abspath $(MYDIR)../ext/zookeeper) $(abspath $(MYDIR)../ext/lwip/api) $(abspath $(MYDIR)../ext/lwip/core)
 EBBRT_CONFIG := $(abspath $(MYDIR)../src/ebbrtcfg.h) 
-EBBRT_APP_CPPFLAGS := -Wno-unused-function -Wno-unused-variable -Wno-write-strings -Wno-sign-compare  -Wno-error 
+EBBRT_APP_CPPFLAGS := 
 EBBRT_APP_CXXFLAGS := 
 
 include $(abspath $(EBBRT_SRCDIR)/apps/ebbrtbaremetal.mk)
