22a71df4325624f11afc6cb5f833a3378edd9850
Jim Cadden <jmcadden@bu.edu>
2017-02-09 14:52:17 -0500

    app(weaksym): a working (but broken) weak GidMap

---
+++ apps/weaksym/src/baremetal/weaksym.cc
@@ -0,0 +1,33 @@
+//          Copyright Boston University SESA Group 2013 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <ebbrt/Debug.h>
+#include "../ZKGlobalIdMap.h"
+
+void AppMain() { 
+  
+  ebbrt::kprintf("WEAKSYM BACKEND UP.\n"); 
+  ebbrt::global_id_map->Init().Then([](auto connected) {
+
+    ebbrt::kbugon(connected.Get() == false);
+    ebbrt::kprintf("getting secret\n");
+    auto val = ebbrt::global_id_map->Get(42).Block().Get();
+    ebbrt::kprintf("The secret value is %s\n", val.c_str());
+
+    ebbrt::global_id_map->SetWatcher(
+        42, new ebbrt::GlobalIdMap::WatchEvent(ZOO_CHANGED_EVENT, [val]() {
+          auto val = ebbrt::global_id_map->Get(42).Block().Get();
+          ebbrt::kprintf("The new secret value is %s\n", val.c_str());
+          ebbrt::kprintf("setting new secrets\n");
+          ebbrt::global_id_map->Set(42, "Abc").Block();
+          ebbrt::global_id_map->Set(42, "123").Block();
+          ebbrt::global_id_map->Set(42, "you and me").Block();
+          ebbrt::kprintf("done\n");
+        }));
+    ebbrt::kprintf("setting new secret\n");
+    ebbrt::global_id_map->Set(42, "abab rezah").Block();
+  });
+
+}
---
+++ apps/weaksym/src/hosted/weaksym.cc
@@ -0,0 +1,61 @@
+//          Copyright Boston University SESA Group 2013 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <cstdio>
+#include <signal.h>
+
+#include <boost/filesystem.hpp>
+
+#include "../ZKGlobalIdMap.h"
+#include <ebbrt/Runtime.h>
+#include <ebbrt/StaticIds.h>
+#include <ebbrt/hosted/Context.h>
+#include <ebbrt/hosted/ContextActivation.h>
+#include <ebbrt/hosted/NodeAllocator.h>
+
+
+using namespace std;
+ebbrt::Messenger::NetworkId net_id;
+
+int main(int argc, char **argv) {
+  auto bindir = boost::filesystem::system_complete(argv[0]).parent_path() /
+                "/bm/weaksym.elf32";
+
+  ebbrt::Runtime runtime;
+  ebbrt::Context c(runtime);
+  boost::asio::signal_set sig(c.io_service_, SIGINT);
+  {
+    ebbrt::ContextActivation activation(c);
+
+    // ensure clean quit on ctrl-c
+    sig.async_wait([&c](const boost::system::error_code &ec,
+                        int signal_number) { c.io_service_.stop(); });
+
+    // begin EbbRT context
+    ebbrt::event_manager->Spawn([&]() {
+
+        cout << "My ip is: " << ebbrt::messenger->LocalNetworkId().ToString()
+             << std::endl;
+        ebbrt::global_id_map->Init().Then([bindir](auto connected) {
+          ebbrt::kbugon(connected.Get() == false);
+          auto secret = "Hazer Baba";
+          ebbrt::global_id_map->Set(42, secret).Block();
+          ebbrt::global_id_map->SetWatcher(
+              42, new ebbrt::GlobalIdMap::WatchEvent(ZOO_CHANGED_EVENT, []() {
+                ebbrt::kprintf("Value has changed!\n");
+              }));
+          auto ns = ebbrt::node_allocator->AllocateNode(bindir.string());
+          ns.NetworkId().Then(
+              [](ebbrt::Future<ebbrt::Messenger::NetworkId> net_if) {
+                net_id = net_if.Get();
+              });
+        });
+
+    });
+  }
+  c.Run();
+
+  return 0;
+}
